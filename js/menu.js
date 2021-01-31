51450f4ea6bc3b6fc538756de95cd477c095661b
tayinde <tbiayinde@gmail.com>
2021-01-31 15:05:18 -0500

    menu mobile friendliness (hopefully)

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,25 @@ function goToSection(id) {
         location.href = location.pathname + `/../index.html#${id}`
     }
 }
+
+function showPanel() {
+    if (document.getElementById("tabs").style.display == "none" || document.getElementById("tabs").style.display == "") {
+        document.getElementById("tabs").style.display = "inline";
+       var rollDown = setInterval(() => {
+            if (document.getElementById("tabs").clientHeight < 150) {
+                document.getElementById("tabs").style.height = document.getElementById("tabs").clientHeight + 5 + 'px';
+            } else {
+                clearInterval(rollDown);
+            }
+        }, 5);
+    } else {
+        var rollUp = setInterval(() => {
+            if (document.getElementById("tabs").clientHeight > 0) {
+                document.getElementById("tabs").style.height = document.getElementById("tabs").clientHeight - 5 + 'px';
+            } else {
+                document.getElementById("tabs").style.display = "none";
+                clearInterval(rollUp)
+            }
+        }, 5);
+    }
+};
