b474b4ed466ae9ec2866caf45cfdf3aa1c85469a
tayinde <tbiayinde@gmail.com>
2021-03-07 16:00:22 -0500

    minor

--- js/fade-in.js
+++ js/fade-in.js
@@ -7,19 +7,28 @@ var sections = [
 ]
 $(window).scroll(() => {
     sections.forEach((section) => {
-        if ($(window).scrollTop() > $(section).offset().top - 450) {
-            $(section).css('opacity', 1);
-            $(section).css('left', '0')
+        if (($(window).scrollTop() > $(section).offset().top - 450)) {
+            if ($(section).css('opacity') == 0 || $(section).css('left') == '-200px') {
+                $(section).css('opacity', 1);
+                $(section).css('left', '0')
+            }
         } else {
-            $(section).css('opacity', 0);
-            $(section).css('left', '-200px')
+            if ($(section).css('opacity') == 1 || $(section).css('left') == 0) {
+                $(section).css('opacity', 0);
+                $(section).css('left', '-200px')
+            }
         }
     })
-    if ($(window).scrollTop() > $('#contact').offset().top - 550) {
-        $('#contact').css('opacity', 1);
-        $('#contact').css('left', '0')
+
+    if (($(window).scrollTop() > $('#contact').offset().top - 550)) {
+        if ($('#contact').css('opacity') == 0 || $('#contact').css('left') == '-200px') {
+            $('#contact').css('opacity', 1);
+            $('#contact').css('left', '0')
+        }
     } else {
-        $('#contact').css('opacity', 0);
-        $('#contact').css('left', '-200px')
+        if ($('#contact').css('opacity') == 1 || $('#contact').css('left') == 0) {
+            $('#contact').css('opacity', 0);
+            $('#contact').css('left', '-200px')
+        }
     }
 })
