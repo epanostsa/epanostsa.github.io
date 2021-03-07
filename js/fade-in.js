961953607c1229a2d9abba43c47fd90de8dfc3b5
tayinde <tbiayinde@gmail.com>
2021-03-07 16:19:19 -0500

    fixed slide in sometimes making elements not show

--- js/fade-in.js
+++ js/fade-in.js
@@ -8,7 +8,7 @@ var sections = [
 $(window).scroll(() => {
     sections.forEach((section) => {
         try {
-            if (($(window).scrollTop() > $(section).offset().top - 450)) {
+            if (($(window).scrollTop() > $(section).offset().top - 500)) {
                 if ($(section).css('opacity') == 0 || $(section).css('left') == '-200px') {
                     $(section).css('opacity', 1);
                     $(section).css('left', '0')
@@ -34,3 +34,32 @@ $(window).scroll(() => {
         }
     }
 })
+
+setInterval(() => {
+    sections.forEach((section) => {
+        try {
+            if (($(window).scrollTop() > $(section).offset().top - 500)) {
+                if ($(section).css('opacity') == 0 || $(section).css('left') == '-200px') {
+                    $(section).css('opacity', 1);
+                    $(section).css('left', '0')
+                }
+            } else {
+                if ($(section).css('opacity') == 1 || $(section).css('left') == 0) {
+                    $(section).css('opacity', 0);
+                    $(section).css('left', '-200px')
+                }
+            }
+        } catch {}
+    })
+    if (($(window).scrollTop() > $('#contact').offset().top - 550)) {
+        if ($('#contact').css('opacity') == 0 || $('#contact').css('left') == '-200px') {
+            $('#contact').css('opacity', 1);
+            $('#contact').css('left', '0')
+        }
+    } else {
+        if ($('#contact').css('opacity') == 1 || $('#contact').css('left') == 0) {
+            $('#contact').css('opacity', 0);
+            $('#contact').css('left', '-200px')
+        }
+    }
+}, 300)
