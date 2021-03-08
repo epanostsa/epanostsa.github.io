c975e25033570e76073346c8da83b7b4b5b5579c
tayinde <tbiayinde@gmail.com>
2021-03-07 23:31:57 -0500

    minor

--- js/fade-in.js
+++ js/fade-in.js
@@ -1,65 +1,68 @@
-var sections = [
+var margin = 250
+
+var sectionsLeft = [
     '#appointment-description',
-    '#bot-description',
     '#doctor-description',
-    '#diagnosis-description',
-    '#newsletter'
+    '#newsletter',
+    '#about-description',
+    '#ref-content-surround'
 ]
-$(window).scroll(() => {
-    sections.forEach((section) => {
+
+var sectionsRight = [
+    '#bot-description',
+    '#diagnosis-description'
+]
+
+function fadeIn() {
+    sectionsLeft.forEach((section) => {
         try {
-            if (($(window).scrollTop() > $(section).offset().top - 500)) {
-                if ($(section).css('opacity') == 0 || $(section).css('left') == '-200px') {
+            if (($(window).scrollTop() > $(section).offset().top - (window.innerHeight - margin))) {
+                if ($(section).css('opacity') == 0 || $(section).css('margin-left') == '-200px') {
                     $(section).css('opacity', 1);
-                    $(section).css('left', '0')
+                    $(section).css('margin-left', '0px')
                 }
             } else {
-                if ($(section).css('opacity') == 1 || $(section).css('left') == 0) {
+                if ($(section).css('opacity') == 1 || $(section).css('margin-left') == '0px') {
                     $(section).css('opacity', 0);
-                    $(section).css('left', '-200px')
+                    $(section).css('margin-left', '-200px')
                 }
             }
         } catch {}
     })
 
-    if (($(window).scrollTop() > $('#contact').offset().top - 550)) {
-        if ($('#contact').css('opacity') == 0 || $('#contact').css('left') == '-200px') {
+    sectionsRight.forEach((section) => {
+        try {
+            if (($(window).scrollTop() > $(section).offset().top - (window.innerHeight - margin))) {
+                if ($(section).css('opacity') == 0 || $(section).css('margin-right') == '-200px') {
+                    $(section).css('opacity', 1);
+                    $(section).css('margin-right', '0px')
+                }
+            } else {
+                if ($(section).css('opacity') == 1 || $(section).css('margin-right') == '0px') {
+                    $(section).css('opacity', 0);
+                    $(section).css('margin-right', '-200px')
+                }
+            }
+        } catch {}
+    })
+
+    if (($(window).scrollTop() > $('#contact').offset().top - (window.innerHeight - (margin - 100)))) {
+        if ($('#contact').css('opacity') == 0 || $('#contact').css('margin-right') == '-200px') {
             $('#contact').css('opacity', 1);
-            $('#contact').css('left', '0')
+            $('#contact').css('margin-right', '0px')
         }
     } else {
-        if ($('#contact').css('opacity') == 1 || $('#contact').css('left') == 0) {
+        if ($('#contact').css('opacity') == 1 || $('#contact').css('margin-right') == '0px') {
             $('#contact').css('opacity', 0);
-            $('#contact').css('left', '-200px')
+            $('#contact').css('margin-right', '-200px')
         }
     }
+}
+
+$(window).scroll(() => {
+    fadeIn();
 })
 
 setInterval(() => {
-    sections.forEach((section) => {
-        try {
-            if (($(window).scrollTop() > $(section).offset().top - 500)) {
-                if ($(section).css('opacity') == 0 || $(section).css('left') == '-200px') {
-                    $(section).css('opacity', 1);
-                    $(section).css('left', '0')
-                }
-            } else {
-                if ($(section).css('opacity') == 1 || $(section).css('left') == 0) {
-                    $(section).css('opacity', 0);
-                    $(section).css('left', '-200px')
-                }
-            }
-        } catch {}
-    })
-    if (($(window).scrollTop() > $('#contact').offset().top - 550)) {
-        if ($('#contact').css('opacity') == 0 || $('#contact').css('left') == '-200px') {
-            $('#contact').css('opacity', 1);
-            $('#contact').css('left', '0')
-        }
-    } else {
-        if ($('#contact').css('opacity') == 1 || $('#contact').css('left') == 0) {
-            $('#contact').css('opacity', 0);
-            $('#contact').css('left', '-200px')
-        }
-    }
+    fadeIn();
 }, 300)
--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
-var defaultColor = "#6a7794"
+var defaultMenuColor = "white"
+var defaultMenuColor2 = "#3b4252ff"
+
 var onHomePage = location.pathname == "/" || location.pathname.indexOf("index") > -1
 
 function goToSection(id) {
     if (location.pathname.indexOf("index") != -1 || location.pathname == "/") {
         hidePanel()
-        document.getElementById(id).scrollIntoView({behavior: "smooth"});
+        $('html').scrollTop($('#' + id).offset().top);
     } else if (location.pathname.indexOf("newsletter") != -1) {
         if (location.pathname.indexOf("github") == -1) location.href = location.pathname + `/../../#${id}`;
         else location.href = location.pathname + `/../../index.html#${id}`;
@@ -18,15 +20,17 @@ function hidePanel() {
     if ($(window).scrollTop() < 550 && onHomePage) {
         $('#panel').css('background', 'transparent')
         $('#panel').css('box-shadow', 'none')
+        $('.tab').css('color', defaultMenuColor)
     }
     $('#tabs').css('height', '0%')
 }
 
 function showPanel() {
-    $('#panel').css('background', defaultColor)
+    $('#panel').css('background', defaultMenuColor)
     $('#panel').css('box-shadow', '0px 0px 15px rgb(63, 63, 63)')
     $('.tab').css('color', 'white')
-    $('#tabs').css('background', defaultColor)
+    $('#tabs').css('background', defaultMenuColor)
+    $('.tab').css('color', defaultMenuColor2)
     if ($('#tabs').height() == 0) {
         $('#tabs').css('height', '100%')
     } else {
@@ -41,24 +45,41 @@ document.addEventListener('click', (event) => {
 })
 
 setInterval(() => {
-    if (document.body.clientWidth > 1025) {
+    if (document.body.clientWidth > 1025 && $('#tabs').css('height') != '0%') {
         $('#tabs').css('height', '0%');
     }
 }, 100)
 
 if (location.pathname != "/" && location.pathname.indexOf("index") == -1) {
-    $('#panel').css('background', defaultColor)
+    $('#panel').css('background', defaultMenuColor)
     $('#panel').css('box-shadow', '0px 0px 15px rgb(63, 63, 63)')
 }
 
+if (!onHomePage) {
+    $('.tab, .name').css('color', defaultMenuColor2)
+}
+
 $(window).scroll(() => {
-    if (onHomePage) {
-        if ($(window).scrollTop() < 550) {
-            $('#panel').css('background', 'transparent')
-            $('#panel').css('box-shadow', 'none')
-        } else {
-            $('#panel').css('background', defaultColor)
-            $('#panel').css('box-shadow', '0px 0px 15px rgb(63, 63, 63)')
-        }
+    if ($(window).scrollTop() < 550 && onHomePage) {
+        $('#panel').css('background', 'transparent')
+        $('#panel').css('box-shadow', 'none')
+        $('.tab, .name').css('color', defaultMenuColor)
+        $('#show-panel-button').css('filter', 'invert()')
+    } else {
+        $('#panel').css('background', defaultMenuColor)
+        $('#panel').css('box-shadow', '0px 0px 15px rgb(63, 63, 63)')
+        $('.tab, .name').css('color', defaultMenuColor2)
+        $('#show-panel-button').css('filter', 'invert(20%)')
     }
 })
+
+$(".tab").on({
+    mouseenter: () => {
+        $(this).css('color', 'white')
+    },
+    mouseleave: () => {
+        if ($(window).scrollTop() > 550) {
+            $(this).css('color', defaultMenuColor2)
+        }
+    }
+});
