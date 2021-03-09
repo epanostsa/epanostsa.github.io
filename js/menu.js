b91060d3b946e70cb65665f4e311302ccefe11de
tayinde <tbiayinde@gmail.com>
2021-03-09 14:04:26 -0500

    change in wavy design, all pages have transparent to opaque taskbar transition with the transition specialized for each page

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,51 @@
 var defaultMenuColor = "white"
 var defaultMenuColor2 = "#3b4252ff"
+var setMenuToDefault = false;
 
 var onHomePage = location.pathname == "/" || location.pathname.indexOf("index") > -1
+var dontDefaultMenuColor = [
+    'about',
+    'newsletters',
+    'assistant',
+    'appointment-booked'
+]
+
+var dontDefaultMenuColorLight = [
+    'references',
+    'doctors',
+    'appointment.'
+]
+
+function onPage(page) {
+    return location.pathname.indexOf(page) > -1
+}
+
+var onUnchangedMenuPage = false;
+dontDefaultMenuColor.forEach((p) => {
+    if (onPage(p)) {
+        onUnchangedMenuPage = true;
+    }
+})
+
+var onUnchangedMenuLightPage = false;
+dontDefaultMenuColorLight.forEach((p) => {
+    if (onPage(p)) {
+        onUnchangedMenuLightPage = true;
+    }
+})
+
+if (!onUnchangedMenuPage && !onHomePage && !onUnchangedMenuLightPage) {
+    setMenuToDefault = true;
+}
+
+if (setMenuToDefault == true) {
+    $('.tab, .name').css('color', defaultMenuColor2)
+}
+
+if (onUnchangedMenuLightPage) {
+    $('.tab, .name').css('color', defaultMenuColor2)
+    $('#show-panel-button').css('filter', 'invert(0%)')
+}
 
 function hidePanel() {
     if ($(window).scrollTop() < 550 && onHomePage) {
@@ -9,7 +53,18 @@ function hidePanel() {
         $('#panel').css('box-shadow', 'none')
         $('.tab, .name').css('color', defaultMenuColor)
         $('#show-panel-button').css('filter', 'invert()')
+    } else if ($(window).scrollTop() < 250 && onUnchangedMenuPage) {
+        $('#panel').css('background', 'transparent')
+        $('#panel').css('box-shadow', 'none')
+        $('.tab, .name').css('color', defaultMenuColor)
+        $('#show-panel-button').css('filter', 'invert()')
     }
+    else if ($(window).scrollTop() < 250 && onUnchangedMenuLightPage) {
+        $('#panel').css('background', 'transparent')
+        $('#panel').css('box-shadow', 'none')
+        $('.tab, .name').css('color', defaultMenuColor2)
+    }
+
     $('#tabs').css('height', '0%')
     $('body, html').css('overflow-y', 'scroll')
 }
@@ -29,10 +84,10 @@ function showPanel() {
 };
 
 function goToSection(id) {
-    if (location.pathname.indexOf("index") != -1 || location.pathname == "/") {
+    if (onHomePage) {
         hidePanel()
         $('html').scrollTop($('#' + id).offset().top);
-    } else if (location.pathname.indexOf("newsletter") != -1) {
+    } else if (onPage('newsletter')) {
         if (location.pathname.indexOf("github") == -1) location.href = location.pathname + `/../../#${id}`;
         else location.href = location.pathname + `/../../index.html#${id}`;
     } else {
@@ -53,13 +108,9 @@ setInterval(() => {
     }
 }, 100)
 
-if (location.pathname != "/" && location.pathname.indexOf("index") == -1) {
+if (!onHomePage && !onUnchangedMenuPage && !onUnchangedMenuLightPage) {
     $('#panel').css('background', defaultMenuColor)
     $('#panel').css('box-shadow', '0px 0px 15px rgb(63, 63, 63)')
-}
-
-if (!onHomePage) {
-    $('.tab, .name').css('color', defaultMenuColor2)
 }
 
 $(window).scroll(() => {
@@ -68,6 +119,15 @@ $(window).scroll(() => {
         $('#panel').css('box-shadow', 'none')
         $('.tab, .name').css('color', defaultMenuColor)
         $('#show-panel-button').css('filter', 'invert()')
+    } else if ($(window).scrollTop() < 250 && onUnchangedMenuPage) {
+        $('#panel').css('background', 'transparent')
+        $('#panel').css('box-shadow', 'none')
+        $('.tab, .name').css('color', defaultMenuColor)
+        $('#show-panel-button').css('filter', 'invert()')
+    } else if ($(window).scrollTop() < 250 && onUnchangedMenuLightPage) {
+        $('#panel').css('background', 'transparent')
+        $('#panel').css('box-shadow', 'none')
+        $('.tab, .name').css('color', defaultMenuColor2)
     } else {
         $('#panel').css('background', defaultMenuColor)
         $('#panel').css('box-shadow', '0px 0px 15px rgb(63, 63, 63)')
@@ -75,14 +135,3 @@ $(window).scroll(() => {
         $('#show-panel-button').css('filter', 'invert(20%)')
     }
 })
-
-$(".tab").on({
-    mouseenter: () => {
-        $(this).css('color', 'white')
-    },
-    mouseleave: () => {
-        if ($(window).scrollTop() > 550) {
-            $(this).css('color', defaultMenuColor2)
-        }
-    }
-});
