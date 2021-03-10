95ecb30ad9a3339c48f748c80e9ac29df94cd128
tayinde <tbiayinde@gmail.com>
2021-03-09 20:34:11 -0500

    minor

--- js/fade-in.js
+++ js/fade-in.js
@@ -55,14 +55,29 @@ function fadeIn() {
             if (($(window).scrollTop() > $(section).offset().top - (window.innerHeight - margin))) {
                 if ($(section).css('opacity') == 0 || $(section).css('opacity') == 0) {
                     $(section).css('opacity', 1);
-                    $(section).css('margin-left', 'auto')
                 }
             } else {
-                if ($(section).css('opacity') == 1 || $(section).css('opacity') == 1) {
+                if ($(section).css('opacity') == 1) {
                     $(section).css('opacity', 0);
                 }
             }
         } catch {}
+    
+        $('.doctorbox').each(function( ) {
+            try {
+                if (($(window).scrollTop() > $(this).offset().top - (window.innerHeight - margin))) {
+                    if ($(this).css('opacity') == 0 || $(this).css('opacity') == 0) {
+                        $(this).css('opacity', 1);
+                        $(this).css('transform', 'scale(1, 1)');
+                    }
+                } else {
+                    if ($(this).css('opacity') == 1) {
+                        $(this).css('opacity', 0);
+                        $(this).css('transform', 'scale(1.1, 1.1)');
+                    }
+                }
+            } catch {}
+        })
     })
 
     if (($(window).scrollTop() > $('#contact').offset().top - (window.innerHeight - (margin - 100)))) {
