b0e057b880426dd819676bd3f24db7a58c0daf4a
tayinde <tbiayinde@gmail.com>
2021-04-07 16:16:19 -0400

    image optimizing from anderson and mobile friendliness on symptom checker

--- js/symptom-checker.js
+++ js/symptom-checker.js
@@ -1,3 +1,4 @@
+var onSmallerDevice = window.innerWidth <= 1024;
 function checkSymptoms() {
     var symptoms = [];
     if ($('#head-front').prop('checked') == true) symptoms.push("Head pain (front)")
@@ -27,7 +28,7 @@ function checkSymptoms() {
     $('#summary').val((symptoms.join(', ') || "No symptoms") + " (Verified by Symptom Checker ✅)")
     document.getElementById('summary').select();
     document.execCommand('copy');
-    $('#copied').css('height', '140px')
+    $('#copied').css('height', onSmallerDevice ? '200px' : '140px');
     $(window).scrollTop($('#submit-button').offset().top - 150)
 }
 
