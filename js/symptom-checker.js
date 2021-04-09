0de630f09f05bf7e180f6b028dc27f15b2e4415a
tayinde <tbiayinde@gmail.com>
2021-04-09 15:27:00 -0400

    minor

--- js/symptom-checker.js
+++ js/symptom-checker.js
@@ -28,7 +28,7 @@ function checkSymptoms() {
     $('#summary').val((symptoms.join(', ') || "No symptoms") + " (Verified by Symptom Checker ✅)")
     document.getElementById('summary').select();
     document.execCommand('copy');
-    $('#copied').css('height', onSmallerDevice ? '200px' : '140px');
+    $('#copied').css('height', onSmallerDevice ? '300px' : '240px');
     $(window).scrollTop($('#submit-button').offset().top - 150)
 }
 
